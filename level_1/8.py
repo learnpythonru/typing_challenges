4cdd63241e75275c58104697555c2c097a755f64
abirukov2008 <abirukov2008@gmail.com>
2023-02-16 19:34:45 +0300

    level1

--- level_1/8.py
+++ level_1/8.py
@@ -1,11 +1,10 @@
 import decimal
 import uuid
-from _decimal import Decimal
 
 from constants import ___
 
 
-def get_user_balance(user_id: uuid.UUID) -> Decimal:
+def get_user_balance(user_id: uuid.UUID) -> decimal.Decimal:
     pass
 
 
